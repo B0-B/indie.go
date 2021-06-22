4d4e62be461cd1789dffc3c498ef41a8593c8ef2
B0-B <alch3mist94@protonmail.com>
2021-06-22 22:56:54 +0200

    fixed the load and span of image

--- main.go
+++ main.go
@@ -1,29 +1,72 @@
 /*
-Indie is a steganographic program which hides cypher into images.
+Indie is a steganographic program which hides text into images.
 */
 
 // == packages ==
 package main
+
 import (
-    "os"
-	"fmt"	
-	"image" 	
-    _"image/jpeg"
-    _"image/png" 
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
 )
 
+// == parameters ==
+var __SIZE__ = 1
+
 // == functions ==
-func loadImage (filePath string) (image.Image, error) {
-    f, err := os.Open(filePath)
-    if err != nil {
-        return nil, err
-    }
-    defer f.Close()
-    img, _, err := image.Decode(f)
-    return img, err
+func loadImage(filePath string) (image.Image, image.Config, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("indie loadImage open ERROR:", err)
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	//reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(f))
+	f, err = os.Open(filePath)
+	defer f.Close()
+	conf, _, e := image.DecodeConfig(f)
+	if e != nil {
+		fmt.Println("indie decoding ERROR:", e)
+	}
+	if err != nil {
+		fmt.Println("indie loadImage ERROR:", err)
+	}
+	return img, conf, err
 }
 
-func main () {
-    img,_ := loadImage("indigo.jpeg")
-    fmt.Println(img)
+func spanImage(imageObject image.Image, configObject image.Config) (matrix [][][]uint8) {
+	h, w := configObject.Height, configObject.Width
+	// determine dimensions and init matrix
+	matrix = make([][][]uint8, h)
+	for i := 0; i < h; i++ {
+		matrix[i] = make([][]uint8, w)
+		for j := 0; j < w; j++ {
+			matrix[i][j] = make([]uint8, 3)
+		}
+	}
+
+	// fill matrix
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			fmt.Println(imageObject.At(i, j).RGBA())
+		}
+	}
+
+	// print info
+	capacity := __SIZE__ * h * w / 8
+	fmt.Println(capacity, "Bytes")
+	return
+}
+
+func main() {
+	img, conf, err := loadImage("indigo.jpeg")
+	matrix := spanImage(img, conf)
+	if err != nil {
+		fmt.Println("indie ERROR:", err)
+	}
+	fmt.Println(matrix)
 }
